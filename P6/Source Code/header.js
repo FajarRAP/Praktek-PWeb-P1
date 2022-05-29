67eb3aac715d97b5e13a000a3af24834837eda81
FajarRAP <fajary781@gmail.com>
2022-05-29 19:46:59 +0700

    commit pertama

---
+++ P6/Source Code/header.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h1> UNIVERSITAS AHMAD DAHLAN <h1>");
+document.writeln("<h2> FAKULTAS TEKNOLOGI INDUSTRI <h2>");
+document.writeln("<h3> JURUSAN TEKNIK INFORMATIKA <h3>");
+document.writeln("<hr width=600 color=black");
+document.writeln("/center");
---
+++ P6/Source Code/postest1.js
@@ -0,0 +1,14 @@
+var a = "100";
+var b = "200";
+var c = 300;
+var d = 400;
+var e = a + b; //string + string
+document.writeln("<pre>");
+document.writeln("100 + 200 = " + e);
+
+e = b + c; //string + numeric
+document.writeln("200 + 300 = " + e);
+
+e = c + d; //numeric + numeric
+document.writeln("300 + 400 = " + e);
+document.writeln("<pre>");
